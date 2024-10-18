75576be071ff9ca5314a11ac2fe7c0cca9382fc6
Dimitri Almon <dimitri.almon@gmail.com>
2024-10-18 19:20:41 +0200

    Initial Commit

---
+++ iPodSimulator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iPodSimulator
+//
+//  Created by Dimitri ALMON on 18/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iPodSimulator/iPodSimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPodSimulatorApp.swift
+//  iPodSimulator
+//
+//  Created by Dimitri ALMON on 18/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct iPodSimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
